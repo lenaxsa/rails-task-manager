998c4c428478d661f9c6f2721ebc9ea46cf35af6
Lena Sachse <lena.sachse@t-online.de>
2023-05-15 18:45:59 +0200

    working on 'delete'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  post '/tasks', to: 'tasks#create'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
 end
